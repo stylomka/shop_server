3d988a78280d4d5b47bb1187c8e681ab3d01f89f
stylomka <{ID}+{EmizhAlan}@users.noreply.github.com>
2024-04-19 02:26:23 +0300

    added server

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,44 @@
-import * as session from 'express-session';
-import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { tr } from '@faker-js/faker';
-const cors = require('cors');
-const express = require('express');
-const app = express();
-
-const corsOptions = {
-  origin: 'https://shop-client-de4f.onrender.com'  // Разрешите только запросы с этого источника
-};
-
-app.use(cors(corsOptions));
-
-// Остальная часть вашего кода сервера
+import { SwaggerConfig } from './swagger.config';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: 'keyword',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Настройка CORS
+  app.enableCors({
+    origin: 'https://shop-client-de4f.onrender.com',
+    credentials: true, // если нужны cookies/Credentials
+  });
+
+  // Настройка сессий и Passport
+  app.use(session({
+    secret: 'keyword',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: true, // если вы используете HTTPS
+      maxAge: 86400000 // 24 часа
+    }
+  }));
   app.use(passport.initialize());
   app.use(passport.session());
 
+  // Настройка Swagger
   const config = new DocumentBuilder()
-  .setTitle('Над сервером работал Емиж Алан')
-  .setDescription('api documentation')
-  .setVersion('1.0')
-  .addTag('api')
-  .build();
+    .setTitle('API Documentation')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
-
-
